a53cd2a91ec14b6eb6fb3f4bab99cc7ee74be52d
lalebdi <loai.alebdi@gmail.com>
2021-02-03 12:07:07 -0500

    add the templates

--- src/django_blog/views.py
+++ src/django_blog/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse("<h1>Testing testing</h1>")
+    return HttpResponse(request, "index.html")
+
 
 def about_page(request):
-    return HttpResponse("<h1>About page</h1>")
+    return HttpResponse("<h1>About Us</h1>")
+
 
 def contact_page(request):
-    return HttpResponse("<h1>Contact Page</h1>")
+    return HttpResponse("<h1>Contact Us</h1>")
 
