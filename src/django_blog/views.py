3eba10241040dd2cbcd38203b4ddc49786041d7f
lalebdi <loai.alebdi@gmail.com>
2021-02-03 11:44:33 -0500

    add about and contact view

--- src/django_blog/views.py
+++ src/django_blog/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 def home_page(request):
     return HttpResponse("<h1>Testing testing</h1>")
 
+def about_page(request):
+    return HttpResponse("<h1>About page</h1>")
+
+def contact_page(request):
+    return HttpResponse("<h1>Contact Page</h1>")
+
