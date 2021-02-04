b36a7214c7913c30ce454de5c7b8c8e9e7971eb0
lalebdi <loai.alebdi@gmail.com>
2021-02-04 10:33:30 -0500

    home_page can render any kind of template

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+K
+src/django_blog/settings.py,0/8/0865a5d7d5d3537f729856c35d4bef559bab324e
+H
+src/templates/index.html,7/9/79fdf14e0ea0a4e1232014f3dde750b28eb85932
+G
+src/templates/base.html,5/3/53d43689e8f59a96532a4b7fefe5b3fe7dfbe2e3
+H
+src/templates/about.html,f/0/f05619b141d3de36d38a8a66d6bb2bf7511c65b5
+G
+src/django_blog/urls.py,1/8/1827a7a45bfbd4a20bddb130920772cbf0c8da8d
+H
+src/django_blog/views.py,0/0/0054de68e812ad9ec5e78bf2b785d2059965a402
--- src/django_blog/views.py
+++ src/django_blog/views.py
@@ -5,9 +5,13 @@ from django.template.loader import get_template
 
 def home_page(request):
     my_title = "Hello there..."
+    context = {"title": my_title}
+    template_name = "index.html"
+    template_obj = get_template(template_name)
+    rendered_item = template_obj.render(context)
     # doc = "<h1>{title}</h1>".format(title=my_title)
     # django_rendered_doc = "<h1>{{title}}</h1>".format(title=my_title)
-    return render(request, "index.html", {"title": my_title})
+    return HttpResponse(rendered_item)
 
 
 def about_page(request):
@@ -22,4 +26,5 @@ def example_page(request):
     context = {"title": "Example"}
     template_name = "index.html"
     template_obj = get_template(template_name)
-    return HttpResponse(template_obj.render(context))   #render(request, "index.html", {"title": "Contact Us"})
+    rendered_item = template_obj.render(context)
+    return HttpResponse(rendered_item)
