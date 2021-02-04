40c3c0ae37ba0eaf703680bbec522621f7987715
lalebdi <loai.alebdi@gmail.com>
2021-02-03 20:22:02 -0500

    add the template context processors

--- src/django_blog/views.py
+++ src/django_blog/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 
 def home_page(request):
@@ -16,3 +17,9 @@ def about_page(request):
 def contact_page(request):
     return render(request, "index.html", {"title": "Contact Us"})
 
+
+def example_page(request):
+    context = {"title": "Example"}
+    template_name = "index.html"
+    template_obj = get_template(template_name)
+    return HttpResponse(template_obj.render(context))   #render(request, "index.html", {"title": "Contact Us"})
