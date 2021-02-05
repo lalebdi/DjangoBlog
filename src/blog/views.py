a35e321d91e4487cf2a3183833727415d6a6c291
lalebdi <loai.alebdi@gmail.com>
2021-02-05 10:17:17 -0500

    do the QuerySet lookups

--- src/blog/views.py
+++ src/blog/views.py
@@ -5,8 +5,15 @@ from django.http import Http404
 from .models import BlogPost
 
 
+# Get implies a single object
+# filter implies a list of objects. query sets will help account for the repeated slug problem (Multiple Objects returned)
+
 def blog_post_detail_page(request, slug):
-    obj = get_object_or_404(BlogPost, slug=slug)
+    # queryset = get_object_or_404(BlogPost, slug=slug)
+    queryset = BlogPost.objects.filter(slug=slug)
+    if queryset.count() != 1:  # this will handle the multiple objects returned errors
+        raise Http404
+    obj = queryset.first()
     template_name = "blog_post_detail.html"
     context = {"object": obj}
     return render(request, template_name, context)
