0ab438edc2f5ffcd07663079d0d668cdd87e67a8
lalebdi <loai.alebdi@gmail.com>
2021-02-08 12:34:17 -0500

    validate data on fields

--- src/django_blog/forms.py
+++ src/django_blog/forms.py
@@ -5,3 +5,10 @@ class ContactForm(forms.Form):
     full_name = forms.CharField() #This has to match the name attribute in the input tag
     email = forms.EmailField()
     content = forms.CharField(widget=forms.Textarea)
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get('email')
+        print(email)
+        if email.endswith(".edu"):
+            raise forms.ValidationError("This is not a valid email. Don't use .edu 🙄")
+        return email
