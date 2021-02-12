73500e8034eab499bae8cfa730240b29c90501be
lalebdi <loai.alebdi@gmail.com>
2021-02-12 10:33:31 -0500

    add searches view url path

--- src/django_blog/urls.py
+++ src/django_blog/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 from .views import home_page, about_page, contact_page, example_page
 from blog.views import blog_post_create_view
+from searches.views import search_view
 
 
 
@@ -26,6 +27,7 @@ urlpatterns = [
     path('', home_page),
     path('blog-new/', blog_post_create_view),
     path('blog/', include('blog.urls')),
+    path('search/', search_view),
     path('about/', about_page),
     path('contact/', contact_page),
     path('example/', example_page),
