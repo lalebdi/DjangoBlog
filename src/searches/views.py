bfc32e4bd3b552fd1ba531488a28a9621357c1e8
lalebdi <loai.alebdi@gmail.com>
2021-02-12 10:30:19 -0500

    create searches view function

--- src/searches/views.py
+++ src/searches/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import SearchQuery
+# Create your views here. then add it to the project urls
 
 
 def search_view(request):
+    query = request.GET.get('q', None) # q is the name of the input/ None is for the default value
+    user = None
+    if request.user.is_authenticated:
+        user = request.user
 
-    return render(request, 'searches/view.html', {})
+    SearchQuery.objects.create(user=user, query=query) # this will save the query every time
+    context = {"query": query}
+    return render(request, 'searches/view.html', context)
