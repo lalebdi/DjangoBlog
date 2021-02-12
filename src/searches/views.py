b5097029b88a1f303d6a5b5feb71b8c8392faf5a
lalebdi <loai.alebdi@gmail.com>
2021-02-12 10:39:57 -0500

    fix search function bug

--- src/searches/views.py
+++ src/searches/views.py
@@ -8,7 +8,7 @@ def search_view(request):
     user = None
     if request.user.is_authenticated:
         user = request.user
-
-    SearchQuery.objects.create(user=user, query=query) # this will save the query every time
+    if query is not None:
+        SearchQuery.objects.create(user=user, query=query) # this will save the query every time
     context = {"query": query}
     return render(request, 'searches/view.html', context)
